2bf084a7e55c8eb9401c27b8c42f65951abc500c
GHIOMA <martinezgabrielalejandro4@gmail.com>
2023-01-18 17:18:11 -0300

    advances

--- src/components/ResidentInfo.jsx
+++ src/components/ResidentInfo.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const ResidentInfo = () => {
   return (
     <div>
-      
+      <h2>Nombre: </h2>
+      <h2>Estado: </h2>
+      <h3>Lugar: </h3>
+      <h3>Cantidad de episodios en que aprece: </h3>
     </div>
   );
 };
--- src/pages/CharacterId.jsx
+++ src/pages/CharacterId.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import ButtonNaviagte from '../components/ButtonNaviagte';
+import ResidentInfo from '../components/ResidentInfo';
 import dataApi from '../customHooks/dataApi';
 
 const CharacterId = () => {
@@ -17,6 +18,9 @@ const CharacterId = () => {
     path='/'
   />
 
+  const residentsInfo = <ResidentInfo/>
+
+
   const { data } = dataApi(`https://rickandmortyapi.com/api/location/${characterId}`);
 
  const residents = data.residents;
@@ -28,6 +32,7 @@ const CharacterId = () => {
       {buttonNavigateLocation}
       {buttonNavigateHome}
       <h1>{characterId}</h1>
+      {residentsInfo}
      
 
 
